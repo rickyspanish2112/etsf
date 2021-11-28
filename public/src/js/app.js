5b7676f0b60e5181bfa376104f3549ecb335474d
rickyspanish2112 <ross.starkey@outlook.com>
2021-11-28 20:22:48 +0000

    Got emulator working

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -95,19 +95,29 @@ function configurePushSub() {
     });
 }
 
-
-
 function askForNotificationPermission() {
-  Notification.requestPermission(function(result) {
-    console.log('User Choice', result);
-    if (result !== 'granted') {
-      console.log('No notification permission granted!');
-    } else {
-     // displayConfirmNotification();
-     configurePushSub();
-    }
-  });
+  Notification.requestPermission()
+    .then(function(permission) {
+      console.log('User Choice', permission);
+      if (permission !== 'granted') {
+        console.log('No notification permission granted!');
+      } else {
+        configurePushSub();
+      }
+    });
 }
+
+// function askForNotificationPermission() {
+//   Notification.requestPermission(function(result) {
+//     console.log('User Choice', result);
+//     if (result !== 'granted') {
+//       console.log('No notification permission granted!');
+//     } else {
+//      // displayConfirmNotification();
+//      configurePushSub();
+//     }
+//   });
+// }
 
 if ('Notification' in window) {
   for (var i = 0; i < enableNotificationsButtons.length; i++) {
